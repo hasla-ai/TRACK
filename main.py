c2c791c0da4e719a661ceb329437536fc0a03b01
hrjeon <hrjeon11@gmail.com>
2026-08-17 00:28:08 +0900

    Mission 002: Add player movement

--- main.py
+++ main.py
@@ -11,23 +11,50 @@ pygame.display.set_caption("TRACK")
 
 clock = pygame.time.Clock()
 
+player_x = WIDTH // 2
+player_y = HEIGHT // 2
+
+player_width = 50
+player_height = 50
+
+player_speed = 300
+
 running = True
 
-while running: # Game Loop
-# Input -> Status update -> Rending -> next Frame
-## Input -> Rending -> Input -> Rending (Loop)
-### Input -> Move -> Aim -> Deal -> Calculate -> Rendering
-
+while running:
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
+    keys = pygame.key.get_pressed()
+
+    if keys[pygame.K_w]:
+        player_y -= player_speed / 60
+
+    if keys[pygame.K_s]:
+        player_y += player_speed / 60
+
+    if keys[pygame.K_a]:
+        player_x -= player_speed / 60
+
+    if keys[pygame.K_d]:
+        player_x += player_speed / 60
+
     screen.fill((20, 20, 20))
+
+    player = pygame.Rect(
+        int(player_x),
+        int(player_y),
+        player_width,
+        player_height
+    )
+
+    pygame.draw.rect(screen, (255, 255, 255), player)
 
     pygame.display.flip()
 
-    clock.tick(60)  # Max 60 FPS
+    clock.tick(60)
 
 pygame.quit()
 sys.exit()
