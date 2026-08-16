fc3a439165f2519b0feebda7a76720a12873f823
hrjeon <hrjeon11@gmail.com>
2026-08-17 00:33:59 +0900

    Mission 003: Add mouse camera control

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("TRACK")
 
 clock = pygame.time.Clock()
-
+font = pygame.font.Font(None, 36)   
 player_x = WIDTH // 2
 player_y = HEIGHT // 2
 
@@ -18,6 +18,9 @@ player_width = 50
 player_height = 50
 
 player_speed = 300
+
+mouse_sensitivity = 0.2
+camera_angle = 0
 
 running = True
 
@@ -41,6 +44,10 @@ while running:
     if keys[pygame.K_d]:
         player_x += player_speed / 60
 
+    mouse_x, mouse_y = pygame.mouse.get_rel()
+
+    camera_angle += mouse_x * mouse_sensitivity
+
     screen.fill((20, 20, 20))
 
     player = pygame.Rect(
@@ -51,6 +58,14 @@ while running:
     )
 
     pygame.draw.rect(screen, (255, 255, 255), player)
+
+    angle_text = font.render(
+    f"Camera Angle: {camera_angle:.1f}",
+    True,
+    (255, 255, 255)
+)
+
+    screen.blit(angle_text, (20, 20))
 
     pygame.display.flip()
 
